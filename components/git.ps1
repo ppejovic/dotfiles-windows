f293c94460ba21a6fdb6520f60d989d87670e03a
ppejovic <ppejovic@users.noreply.github.com>
2019-05-23 17:54:50 -0700

    cd to git repo root

--- components/git.ps1
+++ components/git.ps1
@@ -1,4 +1,18 @@
-if (((Get-Command git -ErrorAction SilentlyContinue) -ne $null) -and ((Get-Module -ListAvailable Posh-Git -ErrorAction SilentlyContinue) -ne $null)) {
-  Import-Module Posh-Git
-  Start-SshAgent -Quiet
+if ($null -ne (Get-Command git -ErrorAction SilentlyContinue)) {
+
+  if($null -ne (Get-Module -ListAvailable Posh-Git -ErrorAction SilentlyContinue)) {
+    Import-Module Posh-Git
+    Start-SshAgent -Quiet
+  }
+
+  function Set-LocationToGitRoot ([string] $solutionFile) {
+    $root = (git rev-parse --show-toplevel)
+    
+    if($null -ne $root) {
+      Set-Location $root
+    }
+  }
+  
+  Set-Alias -name cdr -Value Set-LocationToGitRoot
+
 }
